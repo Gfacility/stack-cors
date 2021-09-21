acf720e6821027ccaafdf8b230ffa2c1736c08b7
prashant@gfacility.com <A1#pooja>
2021-09-21 16:23:45 +0530

    log file added

--- src/CorsService.php
+++ src/CorsService.php
@@ -35,15 +35,18 @@ class CorsService
             'maxAge' => 0,
         );
         $requestURL = request()->url();
-        if ((config('app.server') !== 'local') && str_contains($requestURL, 'api/visitor-kiosk/v1') || str_contains($requestURL, 'api/room-signage/v1')) {
+        if ((config('app.server') !== 'local') && (str_contains($requestURL, 'api/visitor-kiosk/v1') || str_contains($requestURL, 'api/room-signage/v1'))) {
             if (config('app.server') == 'prod') {
                 $options['allowedOrigins'] = config('cors.device_environment_origins')['prod'];
             } elseif (config('app.server') == 'uat') {
                 $options['allowedOrigins'] = config('cors.device_environment_origins')['uat'];
             } else {
-                $options['allowedOrigins'] = config('cors.device_environment_origins')['dev'];;
+                $options['allowedOrigins'] = config('cors.device_environment_origins')['dev'];
             }
         }
+        logger($requestURL);
+        logger( config('app.server'));
+        logger("allowedOrigins ", $options['allowedOrigins']);
         // normalize array('*') to true
         if (in_array('*', $options['allowedOrigins'])) {
             $options['allowedOrigins'] = true;
@@ -147,6 +150,7 @@ class CorsService
 
     private function configureAllowedOrigin(Response $response, Request $request)
     {
+        logger($this->options['allowedOrigins']);
         if ($this->options['allowedOrigins'] === true && !$this->options['supportsCredentials']) {
             // Safe+cacheable, allow everything
             $response->headers->set('Access-Control-Allow-Origin', '*');
